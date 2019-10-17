5a19ef8494c6ffc1a07e7fe983fd43df9c18f048
Stegosauruss <russell.wind@gmail.com>
2019-10-17 22:25:49 +0100

    ran through walkthrough on medium

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Amplify, { Auth, Interactions } from 'aws-amplify';
+import { withAuthenticator, ChatBot, AmplifyTheme } from 'aws-amplify-react';
+import aws_exports from './aws-exports'; // specify the location of aws-exports.js file on your project
+Amplify.configure(aws_exports);
+const myTheme = {
+  ...AmplifyTheme,
+  sectionHeader: {
+    ...AmplifyTheme.sectionHeader,
+    backgroundColor: '#222'
+  }
+};
+class App extends Component {
+  
+  handleComplete(err, confirmation) {
+    if (err) {
+      alert('Bot conversation failed')
+      return;
+    }
+alert('Success: ' + JSON.stringify(confirmation, null, 2));
+    return 'Appointment booked. Thank you! What would you like to do next?';
+  }
+  
+  render() {
+    return (
+      <div className="App">
+          <ChatBot
+          title="My React Bot"
+          theme={myTheme}
+          botName="MakeAppointmentMOBILEHUB"
+          welcomeMessage="Welcome, how can I help you today?"
+          onComplete={this.handleComplete.bind(this)}
+          clearOnComplete={true}
+        />
+      </div>
+    );
+  }
 }
-
-export default App;
+export default withAuthenticator(App, { includeGreetings: true });
